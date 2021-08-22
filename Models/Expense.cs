3a2860ce465f0e56b08998119bd242fb5acb5f7f
Joshua0838 <cosilon@yahoo.com.tw>
2021-08-22 15:30:40 +0800

    增加 Expense using to DataAnnotation

--- Models/Expense.cs
+++ Models/Expense.cs
@@ -1,8 +1,12 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace HomeApp.Models
 {
     public class Expense
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
         public DateTime Date { get; set; }
         public string ConsumptionMethod { get; set; }
